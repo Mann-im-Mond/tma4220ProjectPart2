8d2d2abf570f3bf944bdbf4a5a755ee51122785a
Simon Tobias Werner Thomä <stthoma@its-015-03.idi.ntnu.no>
2017-11-06 16:01:46 +0100

    Added the mesh class with unittests.

---
+++ unittests/meshTest.m
@@ -0,0 +1,71 @@
+addpath('../utils/')
+%{
+Work with the following easy mesh.
+ (0,2)  ___________ (2,2) 
+        |\       /|
+        |  \   /  |
+        |    X    | (1,1)
+        |  /   \  |
+ (0,0)  |/_______\| (2,0)
+%}
+points = [0,0;2,0;1,1;0,2;2,2];
+triangles = [1,2,3;1,3,4;2,3,5;3,4,5];
+success = true;
+mesh = Mesh(triangles, points);
+boundaryEdges = mesh.boundaryEdges();
+realBoundaryEdges = [1,2;1,4;2,5;4,5];
+
+neumannIdentifier = @(x) (x(1)>.5);
+realDirichletNodes = [1;4];
+dirichletNodes = mesh.dirichletNodes(neumannIdentifier);
+realNeumannFaces = [2,5];
+neumannFaces = mesh.neumannFaces(neumannIdentifier);
+
+% TODO we can summarize the following in a more general test function.
+
+%Test whether we found the correct boundary edges.
+for edge=boundaryEdges'
+    C=intersect(perms(edge),realBoundaryEdges,'rows');
+    if isempty(C)
+        disp('We found wrong boundary edges!')
+        success = false;
+    end
+end
+for edge=realBoundaryEdges'
+    C=intersect(perms(edge),boundaryEdges,'rows');
+    if isempty(C)
+        disp('We did not find all boundary edges!')
+        success = false;
+    end
+end
+
+%Test whether we found the correct Neumann faces.
+for edge=neumannFaces'
+    C=intersect(perms(edge),realNeumannFaces,'rows');
+    if isempty(C)
+        disp('We found wrong Neumann faces!')
+        success = false;
+    end
+end
+for edge=realNeumannFaces'
+    C=intersect(perms(edge),neumannFaces,'rows');
+    if isempty(C)
+        disp('We did not find all Neumann faces!')
+        success = false;
+    end
+end
+
+%Test whether we found the correct Dirichlet nodes.
+if(not(and(length(dirichletNodes) == length(realDirichletNodes), ...
+    length(intersect(dirichletNodes,realDirichletNodes) ...
+    ==length(realDirichletNodes)))))
+    disp('The dirichlet nodes are not found correctly.')
+    success = false;
+end
+
+
+if success
+    disp('[unittest/meshTest] succeeded!')
+else
+    disp('[unittest/meshTest] failed!')
+end
---
+++ utils/Mesh.m
@@ -0,0 +1,70 @@
+classdef Mesh
+  properties
+    triangulation
+    points
+  end
+  methods
+    %tri_in is a list of triangles, where the points are given by the index
+    % of the point in the list of points p_in
+    function obj = Mesh(tri_in,p_in)
+      obj.triangulation = tri_in;
+      obj.points = p_in;
+    end
+    
+    %returns the cornerpoints of a triangle (or higher dim equivalent)
+    % given by the indices
+    function cor = corners(obj,triangle)
+      cor = obj.points(triangle,:);
+    end
+
+    %returns all edges, which lay on the boundary of the mesh
+    function be = boundaryEdges(obj)
+      TR = triangulation(obj.triangulation, obj.points);
+      be = freeBoundary(TR);
+    end
+
+    %returns all neumann faces (they are of dimension dim-1)
+    % the neumann identifier is a function, which returns true, iff
+    % the input point is in the neumann Boundary and false if not
+    % WARNING: neumannIdentifier can do anything with points, that are not
+    % on the boundary at all!
+    function ne = neumannFaces(obj, neumannIdentifier)
+      be = obj.boundaryEdges();
+      ne = [];
+      for edge=be'
+        edge = edge';
+        tester = true;
+        for v = edge
+          p = obj.points(v,:);
+          tester = and(tester,neumannIdentifier(p));
+        end
+        if(tester)
+          ne = [ne;edge];
+        end
+      end
+    end
+    
+    %returns all nodes on the dirichlet boundary
+    % the neumann identifier is a function, which returns true, iff
+    % the input point is in the neumann Boundary and false if not
+    % WARNING: neumannIdentifier can do anything with points, that are not
+    % on the boundary at all!
+    function dn = dirichletNodes(obj, neumannIdentifier)
+      dn = [];
+      points = obj.points;
+      be = obj.boundaryEdges();
+      for edge=be'
+        for v=edge'
+          if(not(neumannIdentifier(points(v,:))))
+            if not(ismember(v,dn))
+              dn = [dn,v];
+            end
+          end
+        end
+      end
+    end
+    function d = dim(obj)
+      d = length(obj.points(1,:));
+    end
+  end
+end
