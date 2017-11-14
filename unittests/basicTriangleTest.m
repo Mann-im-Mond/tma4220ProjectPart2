2fc65bb2e2c4dd81b02e5042ac2e81a7c90e8ce9
stthoma <simon.thomae@outlook.de>
2017-11-14 18:59:44 +0100

    added a function, which can calculate integral phi_i*phi_j to the BasicTriangle

--- unittests/basicTriangleTest.m
+++ unittests/basicTriangleTest.m
@@ -1,9 +1,25 @@
 addpath('../utils/')
-tri = BasicTriangle(3);
+addpath('../helperFunctions')
+dim = 3;
+tri = BasicTriangle(dim);
 M = [1,0,0;0,1,0;0,0,1;0,0,0];
 D = [1,0,0;0,1,0;0,0,1;-1,-1,-1]';
 cornerPoints = tri.getCornerPoints();
 partialDerivative = tri.getPDEOfbasisFunction();
+
+quadFunc = QuadratFunctionGenerator().getQuadratureFunction(dim);
+integrals = tri.productBasisFunctionIntegralMatrix();
+realIntegrals = zeros(dim+1);
+
+for i=1:dim+1
+    for j=1:dim+1
+        phi_i = tri.getBasisFunction(i);
+        phi_j = tri.getBasisFunction(j);
+        eval = @(x) phi_i(x).*phi_j(x);
+        realIntegrals(i,j)=quadFunc(cornerPoints,eval);
+    end
+end
+
 success = true;
 if not(isequal(cornerPoints, M))
   disp('The corner poits are not read correctly!')
@@ -22,6 +38,11 @@ for i=1:4
             success = false;
         end
     end
+end
+
+if not(equalUpTo(integrals, realIntegrals, 1e-10))
+  disp('The integrals of the basis functions are not computed correctly!')
+  success = false;
 end
 
 if success
--- utils/BasicTriangle.m
+++ utils/BasicTriangle.m
@@ -35,5 +35,53 @@ classdef BasicTriangle
       B = (obj.getCornerPoints())';
       B(:,obj.dim+1) = -ones(obj.dim,1);
     end
+    
+    %returns a matrix, where the entry (i,j) contains:
+    %integral (phi_i * phi_j)
+    function I = productBasisFunctionIntegralMatrix(obj)
+       I = zeros(obj.dim +1);
+       for i = 1:(obj.dim+1)
+           for j = 1:(obj.dim+1)
+               I(i,j) = obj.productBasisFunctionIntegral(i,j);
+           end
+       end
+    end
+    
+    
+    % TODO this can propablz be simplified, as the case d+1 seems to be not
+    % necessary, but I lack the theory for that yet.
+    
+    % returns integral (phi_i * phi_j)
+    function I = productBasisFunctionIntegral(obj,i,j)
+        d = obj.dim;
+        if d<=0
+            error('invalid dimension');
+        end
+        if(or(i==d+1,j==d+1))
+            if(i~=j)
+                I = (1/d-1/(d+1))*1/(factorial(d-1)) ...
+                    - obj.productBasisFunctionIntegral(1,1);
+                if(d>1)
+                    I = I - (d-1)*obj.productBasisFunctionIntegral(1,2);
+                end
+                return
+            else
+                I = 1/(factorial(d)) - 2*d*(1/d-1/(d+1))*1/(factorial(d-1)) ...
+                    + d*obj.productBasisFunctionIntegral(1,1);
+                if(d>1)
+                   I = I + d*(d-1)*obj.productBasisFunctionIntegral(1,2); 
+                end
+                return
+            end
+        else
+            if(i~=j)
+                I = (1/(d+1)-1/(d+2))*(1/(d-1)-1/d)*1/(factorial(d-2));
+                return
+            else
+                I = (1/(d+2)+1/d-2/(d+1))*1/(factorial(d-1));
+                return
+            end
+        end
+    end
   end
 end
