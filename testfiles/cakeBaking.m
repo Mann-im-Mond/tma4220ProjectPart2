090753be9498ae853203bae27f5e6875e6dc60da
stthoma <simon.thomae@outlook.de>
2017-11-18 19:17:22 +0100

    corrected the test and added a logfile. #12

--- testfiles/cakeBaking.m
+++ testfiles/cakeBaking.m
@@ -3,14 +3,18 @@ addpath('../helperFunctions/')
 
 dateTimeStamp = getDateTimeStamp();
 outputFolder = setUpTestFolder('../testruns',dateTimeStamp);
+diary(join([outputFolder '/logfile.log']))
+diary on
 
-neumannIdentifier = @(x) (x(1)*0);
-%mesh = getCakeMesh('../grid', neumannIdentifier, .15);
-alpha = @(x) x(1)*0 + 1e-7;
-timeInterval = TimeInterval(0,1200,.1,600);
-u0 = @(x) x(1)*0 +293;
-gD = @(x,t) norm(x(:)'*[0;0;1],1)*0 +453 +0*t^2;
-gN = @(x,t) norm(x(:)'*[1;0;1],1)*0 +1 +0*t;
+dateTimeStamp
+neumannIdentifier = @(x) (x(1)*0)
+mesh = getCakeMesh('../grid', neumannIdentifier, .15)
+alpha = @(x) x(1)*0 + 1e-7
+timeInterval = TimeInterval(0,1200,.1,600)
+u0 = @(x) x(1)*0 +293
+gD = @(x,t) norm(x(:)'*[0;0;1],1)*0 +453 +0*t^2
+gN = @(x,t) norm(x(:)'*[1;0;1],1)*0 +1 +0*t
 solver = HeatEquationSolver(mesh,timeInterval,alpha,u0,gD,gN);
 u = solver.solve();
 dlmwrite(join([outputFolder '/temperatureSolution.txt']),u);
+diary off
