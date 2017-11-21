4a6e4d4e11eb937edea38a86061ac4adb518578e
hans.pieper <hans.pieper@rwth-aachen.de>
2017-11-21 05:02:39 +0100

    Minor adujustments to Plotter.m

--- utils/Plotter.m
+++ utils/Plotter.m
@@ -24,34 +24,11 @@ classdef Plotter < handle
         u_max
     end
     
-    properties (Access=private)
-        pdeModel
-    end
-    
     methods (Access=public,Static)
         function rgb=colorConverterStatic(u,u_min,u_max)
             to01Convert=@(v) (v-u_min)/(u_max-u_min);
             cC=@(v) [min(2*to01Convert(v),1),min(2*to01Convert(v),2-2*to01Convert(v)),max(1-2*to01Convert(v),0)];
             rgb=cC(u);
-        end
-        
-        function handler=slicePlotStatic(u,tri,plain_points)
-            l=length(tri(:,1));
-            Y=zeros(3,l);
-            Z=zeros(3,l);
-            C=zeros(3,l);
-            %Set the color to blue everywhere
-            C(:,3)=1;
-            %Set the color to blue everywhere
-            for k=1:l
-                Y(:,k)=plain_points(tri(k,:),1);
-                Z(:,k)=plain_points(tri(k,:),2);
-            end
-            for k=1:l
-                tmp=Plotter.colorConverterStatic(u(tri(k,:)),min(u),max(u));
-                C(:,k)=tmp(1,:)';
-            end
-            handler=fill(Y,Z,C);
         end
     end
     
@@ -66,6 +43,7 @@ classdef Plotter < handle
         function rgb=colorConverter(obj,K)
             rgb=Plotter.colorConverterStatic(obj.u(:,K),obj.u_min,obj.u_max);
         end
+        
         function obj=Plotter(u,mesh,timeInterval)
             [N,M]=size(u);
             if not(N==length(mesh.points(:,1)))
@@ -76,9 +54,11 @@ classdef Plotter < handle
             elseif (M==1)
                 timeInterval.t_max=timeInterval.t_0;
                 timeInterval.setDescreteInterval();
-            elseif (M<timeInterval.getNumberOfSteps()+1)
+            elseif (M<timeInterval.n_to_plot+1)
                 timeInterval.t_max=(M-1)*timeInterval.h;
                 timeInterval.setDescreteInterval();
+            elseif (M>timeInterval.n_to_plot+1)
+                error('To many timesteps');
             end
             obj.u=u;
             obj.mesh=mesh;
@@ -120,10 +100,22 @@ classdef Plotter < handle
                 error(['K has to be between 1 and ',length(obj.u(1,:))]);
             end
             handler=scatter3(obj.mesh.points(:,1),obj.mesh.points(:,2),obj.mesh.points(:,3),10.*ones(length(obj.mesh.points(:,1)),1),obj.colorConverter(K),'filled');
+            legend(['t=',num2str(obj.timeInterval.descreteInterval((K-1)*obj.timeInterval.n_to_plot+1))],'Location','northoutside');
         end
         
         function movie=animateScatterPlot(obj)
             figure('Name','Scatter plot animation');
+            title('Scatter plot animation');
+            if(obj.dim==2)
+                xlabel([num2str(min(obj.mesh.points(:,1))),'<= x <= ',num2str(max(obj.mesh.points(:,1)))]); % x-axis label
+                ylabel([num2str(min(obj.mesh.points(:,2))),'<= y <= ',num2str(max(obj.mesh.points(:,2)))]); % y-axis label
+            elseif(obj.dim==3)
+                xlabel([num2str(min(obj.mesh.points(:,1))),'<= x <= ',num2str(max(obj.mesh.points(:,1)))]); % x-axis label
+                ylabel([num2str(min(obj.mesh.points(:,2))),'<= y <= ',num2str(max(obj.mesh.points(:,2)))]); % y-axis label
+                zlabel([num2str(min(obj.mesh.points(:,3))),'<= z <= ',num2str(max(obj.mesh.points(:,3)))]); % z-axis label
+            else
+                error('animateScatterPlot can only plot 2 or 3 dimensions.');
+            end
             obj.scatterPlotSingleTimeStep(1);
             axis tight manual;
             ax = gca;
@@ -145,6 +137,17 @@ classdef Plotter < handle
         %animation will be the bounary of the points where u<=thresholds
         %holds.
             figure('Name','Shrinking plot animation');
+            title('Shrinking plot animation');
+            if(obj.dim==2)
+                xlabel([num2str(min(obj.mesh.points(:,1))),'<= x <= ',num2str(max(obj.mesh.points(:,1)))]); % x-axis label
+                ylabel([num2str(min(obj.mesh.points(:,2))),'<= y <= ',num2str(max(obj.mesh.points(:,2)))]); % y-axis label
+            elseif(obj.dim==3)
+                xlabel([num2str(min(obj.mesh.points(:,1))),'<= x <= ',num2str(max(obj.mesh.points(:,1)))]); % x-axis label
+                ylabel([num2str(min(obj.mesh.points(:,2))),'<= y <= ',num2str(max(obj.mesh.points(:,2)))]); % y-axis label
+                zlabel([num2str(min(obj.mesh.points(:,3))),'<= z <= ',num2str(max(obj.mesh.points(:,3)))]); % z-axis label
+            else
+                error('shrinkingPlot can only plot 2 or 3 dimensions.');
+            end
             obj.shrinkingPlotSingleStep(threshold,1);
             axis tight manual;
             ax = gca;
@@ -217,6 +220,7 @@ classdef Plotter < handle
             tri=triangulation(triangles,points);
             %Find boundry of the triangulation and plot it
             handler=obj.boundaryPlot(tri.freeBoundary());
+            legend(['t=',num2str(obj.timeInterval.descreteInterval((K-1)*obj.timeInterval.n_to_plot+1))],'Location','northoutside');
         end
         
         function handler=boundaryPlot(obj,boundary)
@@ -236,6 +240,7 @@ classdef Plotter < handle
             if(isempty(boundary))
                 %Plot nothing
                 handler=scatter3(obj.mesh.points(1,1),obj.mesh.points(1,2),obj.mesh.points(1,3),'MarkerEdgeColor','white');
+                legend(['t=',num2str(obj.timeInterval.descreteInterval((K-1)*obj.timeInterval.n_to_plot+1))],'Location','northoutside');
                 return;
             end
             faces=zeros(1,length(boundary(:,1)));
@@ -260,6 +265,7 @@ classdef Plotter < handle
             end
             %plot the boundary
             handler=patch('Faces',faces,'Vertices',obj.mesh.points,'EdgeColor','blue','FaceColor','none');
+            legend(['t=',num2str(obj.timeInterval.descreteInterval((K-1)*obj.timeInterval.n_to_plot+1))],'Location','northoutside');
         end
         
         function handler=boundaryPlot3D(obj,boundary)
@@ -268,6 +274,7 @@ classdef Plotter < handle
             if(isempty(boundary))
                 %Plot nothing
                 handler=scatter3(obj.mesh.points(1,1),obj.mesh.points(1,2),obj.mesh.points(1,3),'MarkerEdgeColor','white');
+            legend(['t=',num2str(obj.timeInterval.descreteInterval((K-1)*obj.timeInterval.n_to_plot+1))],'Location','northoutside');
                 return;
             else
                 l=length(boundary(:,1));
@@ -326,7 +333,11 @@ classdef Plotter < handle
                 plainPoints(k,:)=plain.plain(delTri.Points(k,1),delTri.Points(k,2))';
             end
             
-            figure('Name','Slice plot animation');
+            figure('Name','Slice plot animation 2D');
+            title('Slice plot animation 2D');
+            xlabel([num2str(min(obj.mesh.points(:,1))),'<= x <= ',num2str(max(obj.mesh.points(:,1)))]); % x-axis label
+            ylabel([num2str(min(obj.mesh.points(:,2))),'<= y <= ',num2str(max(obj.mesh.points(:,2)))]); % y-axis label
+            
             obj.slicePlotSingleStep2D(delTri,plainPoints,1);
             axis tight manual;
             ax = gca;
@@ -348,6 +359,7 @@ classdef Plotter < handle
             u_test=interpol(plainPoints(:,1),plainPoints(:,2),plainPoints(:,3));
             C=Plotter.colorConverterStatic(u_test,min(u_test),max(u_test));
             handler=patch('Faces',delTri.ConnectivityList,'Vertices',delTri.Points,'FaceColor','flat','FaceVertexCData',C,'EdgeColor','none');
+            legend(['t=',num2str(obj.timeInterval.descreteInterval((K-1)*obj.timeInterval.n_to_plot+1))],'Location','northoutside');
         end
         
         function movie=animateSlicePlot3D(obj,varargin)
@@ -373,8 +385,13 @@ classdef Plotter < handle
                 plainPoints(k,:)=plain.plain(delTri.Points(k,1),delTri.Points(k,2))';
             end
             
-            figure('Name','Slice plot animation');
+            figure('Name','Slice plot animation 3D');
+            title('Slice plot animation 3D');
             view(3);
+            xlabel([num2str(min(obj.mesh.points(:,1))),'<= x <= ',num2str(max(obj.mesh.points(:,1)))]); % x-axis label
+            ylabel([num2str(min(obj.mesh.points(:,2))),'<= y <= ',num2str(max(obj.mesh.points(:,2)))]); % y-axis label
+            zlabel([num2str(min(obj.mesh.points(:,3))),'<= z <= ',num2str(max(obj.mesh.points(:,3)))]); % z-axis label
+            
             obj.slicePlotSingleStep3D(delTri,plainPoints,1);
             axis tight manual;
             ax = gca;
@@ -396,6 +413,7 @@ classdef Plotter < handle
             u_test=interpol(plainPoints(:,1),plainPoints(:,2),plainPoints(:,3));
             C=Plotter.colorConverterStatic(u_test,min(u_test),max(u_test));
             handler=patch('Faces',delTri.ConnectivityList,'Vertices',plainPoints,'FaceColor','flat','FaceVertexCData',C,'EdgeColor','none');
+            legend(['t=',num2str(obj.timeInterval.descreteInterval((K-1)*obj.timeInterval.n_to_plot+1))],'Location','northoutside');
         end
         
         function delTri=getPlainTriangulation(obj,plain,density)
