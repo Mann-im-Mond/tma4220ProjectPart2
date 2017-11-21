f7ac5b5c79ce5fa265cc93a5b3945446e0f80d2d
stthoma <simon.thomae@outlook.de>
2017-11-21 16:39:12 +0100

    plotFromFile now has the movie as output.

--- helperFunctions/plotFromFile.m
+++ helperFunctions/plotFromFile.m
@@ -1,13 +1,13 @@
-function [] = plotFromFile(folderPath,t_max,timeStep,dim)
+function movie = plotFromFile(folderPath,t_max,timeStep,dim)
     u=dlmread([folderPath '/temperatureSolution.txt']);
     mesh = getCakeMesh('../grid', @(x) false, .15);
     plot_num = 60/timeStep;
     timeInterval = TimeInterval(0,t_max,timeStep,plot_num);
     plotter=Plotter(u,mesh,timeInterval);
     if(dim == 2)
-        plotter.animateSlicePlot2D();
+        movie = plotter.animateSlicePlot2D();
     else if(dim == 3)
-        plotter.animateScatterPlot();
-    end
+        movie = plotter.animateScatterPlot();
+    end 
 end
 
