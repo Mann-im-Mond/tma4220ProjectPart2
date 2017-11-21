bbbff58cfdfbaa1d9906b578d00b03935df401da
stthoma <simon.thomae@outlook.de>
2017-11-21 17:04:19 +0100

    added possibility of writing out movies to plotFromFile

--- helperFunctions/plotFromFile.m
+++ helperFunctions/plotFromFile.m
@@ -1,13 +1,21 @@
-function movie = plotFromFile(folderPath,t_max,timeStep,dim)
+function movieOut = plotFromFile(folderPath,t_max,timeStep,dim,outputFile)
     u=dlmread([folderPath '/temperatureSolution.txt']);
     mesh = getCakeMesh('../grid', @(x) false, .15);
     plot_num = 60/timeStep;
     timeInterval = TimeInterval(0,t_max,timeStep,plot_num);
     plotter=Plotter(u,mesh,timeInterval);
     if(dim == 2)
-        movie = plotter.animateSlicePlot2D();
+        movieOut = plotter.animateSlicePlot2D();
     else if(dim == 3)
-        movie = plotter.animateScatterPlot();
+        movieOut = plotter.animateScatterPlot();
     end 
+    if not(isequal(outputFile,''))
+        disp(['writing movie to' outputFile])
+        v = VideoWriter(outputFile);
+        v.FrameRate = 6;
+        open(v);
+        writeVideo(v,movieOut)
+        close(v)
+    end
 end
 
