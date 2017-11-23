3bda92312ba1da14e122a42ec4081089b9a71fc3
stthoma <simon.thomae@outlook.de>
2017-11-23 04:08:50 +0100

    tidied up the testfiles. Removed scripts, which were not working, or would not be used anymore.

--- testfiles/AddPlotsToTestruns.m
+++ testfiles/AddPlotsToTestruns.m
@@ -6,14 +6,21 @@ for runFolder = list'
     if(runName == '.')
         continue
     end
+    if not(contains(runName, 'pureNormalCake'))
+        continue
+    end
     fullPath = [folder '/' runName];
+    t_max = 7200;
     stepSize = .1;
     if(contains(runName,'smallSteps'))
         stepSize = .01;
     end
-    for dim = 2
-        aviName = [runName '_' num2str(dim) 'D.avi'];
+    if contains(runName, 'pure')
+        t_max = 14400;
+    end
+    for dim = [2 2.4 2.6 3]
+        aviName = [runName '_' num2str(dim) 'D_fine.avi'];
         aviPath = [fullPath '/' aviName];
-        plotFromFile(fullPath,7200,1,dim,aviPath);
+        plotFromFile(fullPath,t_max,stepSize,dim,aviPath);
     end
 end
